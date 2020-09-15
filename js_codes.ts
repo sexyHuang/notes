773552a630894cf166e05a5599f9a6a2a39a4d21
huangjianyong <huangjianyong@tuzhanai.com>
2020-09-15 16:39:10 +0800

    feat(js_code): code add

--- js_codes.ts
+++ js_codes.ts
@@ -12,3 +12,11 @@ e
 // 通过正则包含匹配，实现分割字符串且包含分割符
 const splitReg = /(\n)/;
 console.log(targetString.split(splitReg));
+
+/**
+ * for...of..循环数组，获取idx
+ */
+const arr = [1, 2, 3, 4, 5, 6, 7];
+for (let [idx, value] of arr.entries()) {
+  console.log(idx, value);
+}
